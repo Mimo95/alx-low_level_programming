2d260debd1c5b2301f9ddb0a0aeeb9063c369865
Mohamed Hajjoubi <hm041949@gmail.com>
2023-03-16 13:30:13 -0700

    Hello, World, tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+int a;
+char d;
+float f;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		return (0);
+}
