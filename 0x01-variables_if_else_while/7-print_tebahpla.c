7b78e7f4c068a9418454b76502a21704a5e0905c
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-03-18 01:42:21 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    char letter;
-    for (letter = 'z'; letter >= 'a'; letter--)
-    
-        putchar(letter);
-    
-    putchar('\n');
-    return 0;
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
