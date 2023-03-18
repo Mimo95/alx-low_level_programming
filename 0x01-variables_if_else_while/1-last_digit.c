4fc7f015a609b495fab31398e74a050a8f283712
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-03-18 01:56:48 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int n,lastDigit;
+	char letter;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	
-    
-    	int lastDigit = n % 10;
-    
-   	 if (lastDigit > 5) {
-        printf("and is greater than 5\n");
-   	 }
-	 else if (lastDigit == 0) {
-        printf("and is 0\n");
-   	 }
-	 else {
-        printf("and is less than 6 and not 0\n");
-   	 }
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
 	return (0);
 }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priv
