b2b0427e081b02273cf3b5459fa81e1f477b0729
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-06-22 15:34:58 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int r)
 	int n;
 
 	if (r < 0)
-		n = (-r % 10);
+		n = -1 * (r % 10);
 	else
 		n = r % 10;
 
-	_putchar(n  + '0');
-	return (n);
+	_putchar((n%10)  + '0');
+	return (n%10);
 }
