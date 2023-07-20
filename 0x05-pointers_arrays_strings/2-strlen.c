d268173471f3a896b6a87d794a1f00f6f934663f
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-07-20 16:38:27 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *s)
 {
-    int i = 0;
-    int j = 0;
-    while (s[i] != '\0')
-    {
-        j++;
-        i++;
-    }
-    return j;
+int i = 0;
+int j = 0;
+while (s[i] != '\0')
+{
+    j++;
+    i++;
+}
+return j;
 }
