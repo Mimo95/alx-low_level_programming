2776652c73b05e70ac6bb12afe721ca0ad5ad910
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-08-24 15:35:35 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,16 @@
  */
 void rev_string(char *s)
 {
-	int i,f, j = 0;
-    char k;
-    
-    while (s[j] != '\0'){
-        j++;
-    }
-    f=j;
-    for (i = 0; i < f/2; i++){
-        k = s[j-1];
-        s[j-1] = s[i];
-        s[i] = k;
-        j--;
-    }
+int i,f,j = 0;
+char k;
+while (s[j] != '\0'){
+j++;
+}
+f=j;
+for(i = 0; i < f/2; i++){
+k = s[j-1];
+s[j-1] = s[i];
+s[i] = k;
+j--;
+}
 }
